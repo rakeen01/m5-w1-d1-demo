11844a063ac7e90e5b10c0faf12852e985a37abf
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2026-02-21 05:09:02 -0800

    final commit

---
+++ demo6/my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap'; // ADDED
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a
+          className="btn btn-danger"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        {/* <Button color="danger">Danger!</Button> ADDED */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
